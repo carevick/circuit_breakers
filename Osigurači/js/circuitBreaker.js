087cc9a5ab1945e2e125689e5491add1fa8ab655
Kristijan Carević <144437619+carevick@users.noreply.github.com>
2023-09-15 22:57:07 +0200

    Initial commit

---
+++ Osigurači/js/circuitBreaker.js
@@ -0,0 +1,9 @@
+function showModal(dialogId){
+    const dialog = document.getElementById(dialogId)
+    dialog.showModal();
+};
+
+function closeModal(dialogId){
+    const dialog = document.getElementById(dialogId)
+    dialog.close();
+};
